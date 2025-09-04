596882c35d707c980bb26e39e1273e147e285746
Roshan Pal <roshanpal18172@gmail.com>
2025-09-04 06:48:06 +0530

    add logic to prime and even odd

--- templates/CheckNumber.js
+++ templates/CheckNumber.js
@@ -5,3 +5,26 @@ const palindromeCheck = () => {
     document.getElementById('palindromeResult').innerText = result;
 }
 document.getElementById('checkPalindrome').addEventListener('click', palindromeCheck);
+
+const primeCheck = () =>{
+    const num = document.getElementById('primeInput').value;
+    let isPrime = true;
+    if (num <=1) 
+        isPrime = false;
+    else{
+        for(let i=2;i<=Math.sqrt(num);i++){
+            if(num%i===0)
+                isPrime=false;
+        }
+    }
+    const result=isPrime?`${num} is a Prime Number.`:`${num} is not a Prime Number.`;
+    document.getElementById('primeResult').innerText=result;
+}
+document.getElementById('checkPrime').addEventListener('click', primeCheck);
+
+const evenOddCheck = () => {
+    const no = document.getElementById('evenOddInput').value;
+    const result = (no % 2 === 0) ? `${no} is an Even Number.` : `${no} is an Odd Number.`;
+    document.getElementById('evenOddResult').innerText = result;
+}
+document.getElementById('checkEvenOdd').addEventListener('click', evenOddCheck);
